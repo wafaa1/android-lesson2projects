d482565d0326b40164ee6b99d575be3e01174cde
@wafaa1 <wafaaegypt@yahoo.com>
2022-05-06 10:37:46 -0500

    code with database and viewmodel not yet key responds

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -16,4 +16,34 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 // TODO (01) Create the SleepNight class.
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMilli: Long = startTimeMilli,
+
+    @ColumnInfo(name = "quality_rating")
+    var sleepQuality: Int = -1
+    )
+
+////    @PrimaryKey (autoGenerate = true)
+//    @PrimaryKey (autoGenerate = true)
+//    var nightId: Long = 0L,
+//    @ColumnInfo
+//    var startTimeMilli: Long = System.currentTimeMillis(),
+//    @ColumnInfo
+//    var stopTimeMilli: Long = startTimeMilli,
+//    @ColumnInfo
+//    var sleepQuality: Int = -1
+//    )
