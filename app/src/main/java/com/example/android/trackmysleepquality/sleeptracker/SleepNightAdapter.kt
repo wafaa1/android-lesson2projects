63ce3e50bef267715dd751d927290e23f0d463dd
@wafaa1 <wafaaegypt@yahoo.com>
2022-05-09 19:32:54 -0500

    +

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -6,103 +6,35 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
-
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun getItemCount() = data.size
+class SleepNightAdapter : ListAdapter<SleepNight,
+        SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
-//        val res = holder.itemView.context.resources //5 move inside ViewHolder class
-        // 1
-//        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-//        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
-//        holder.qualityImage.setImageResource(when (item.sleepQuality) {
-//            0 -> R.drawable.ic_sleep_0
-//            1 -> R.drawable.ic_sleep_1
-//            2 -> R.drawable.ic_sleep_2
-//            3 -> R.drawable.ic_sleep_3
-//            4 -> R.drawable.ic_sleep_4
-//            5 -> R.drawable.ic_sleep_5
-//            else -> R.drawable.ic_sleep_active
-//        })
-       // bind(holder, item, res) // 2
-//        holder.bind(item, res) // 3
-        holder.bind(item)// 5 take out res from signature
+        val item = getItem(position)
+        holder.bind(item)
     }
-
-//    private fun bind( // 2
-//        holder: ViewHolder,
-//        item: SleepNight,
-//        res: Resources
-//    ) {
-//        holder.sleepLength.text =
-//            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-//        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
-//        holder.qualityImage.setImageResource(
-//            when (item.sleepQuality) {
-//                0 -> R.drawable.ic_sleep_0
-//                1 -> R.drawable.ic_sleep_1
-//                2 -> R.drawable.ic_sleep_2
-//                3 -> R.drawable.ic_sleep_3
-//                4 -> R.drawable.ic_sleep_4
-//                5 -> R.drawable.ic_sleep_5
-//                else -> R.drawable.ic_sleep_active
-//            }
-//        )
-//    }
-
-    // 4 move inside ViewHolder class
-//    private fun ViewHolder.bind( // 3 convert holder from parameter to receiver
-//        item: SleepNight,
-//        res: Resources
-//    ) {
-//        sleepLength.text =
-//            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-//        quality.text = convertNumericQualityToString(item.sleepQuality, res)
-//        qualityImage.setImageResource(
-//            when (item.sleepQuality) {
-//                0 -> R.drawable.ic_sleep_0
-//                1 -> R.drawable.ic_sleep_1
-//                2 -> R.drawable.ic_sleep_2
-//                3 -> R.drawable.ic_sleep_3
-//                4 -> R.drawable.ic_sleep_4
-//                5 -> R.drawable.ic_sleep_5
-//                else -> R.drawable.ic_sleep_active
-//            }
-//        )
-//    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.list_item_sleep_night, parent, false)
-
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
 
-        // 4 moved here and removed modifier private and ViewHolder.bind -> bind
-        fun bind( // 3 convert holder from parameter to receiver
+        fun bind(
             item: SleepNight
-//            ,  res: Resources // 5
         ) {
-            val res = itemView.context.resources // 5 moved from onBindViewHolder() and removed holder.
+            val res = itemView.context.resources
 
             sleepLength.text =
                 convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
@@ -119,6 +51,31 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 }
             )
         }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                    .inflate(R.layout.list_item_sleep_night, parent, false)
+                return ViewHolder(view)
+            }
+        }
+    }
+}
+
+/**
+ * Callback for calculating the diff between two non-null items in a list.
+ *
+ * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+ * list that's been passed to `submitList`.
+ */
+class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>(){
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem.nightId == newItem.nightId
     }
 
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem == newItem
+    }
 }
+
