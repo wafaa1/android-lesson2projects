5ce522a7d4f7a5ad1d1428b4013b165c1951b2fc
@wafaa1 <wafaaegypt@yahoo.com>
2022-05-14 10:45:05 -0500

    add ? * 2 in BindingAdapter for ImageView to resolve crashing when going to details fragment

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -9,8 +9,8 @@ import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
 @BindingAdapter("sleepImage")
-fun ImageView.setSleepImage(item: SleepNight) {
-    setImageResource(when (item.sleepQuality) {
+fun ImageView.setSleepImage(item: SleepNight?) {
+    setImageResource(when (item?.sleepQuality) {
         0 -> R.drawable.ic_sleep_0
         1 -> R.drawable.ic_sleep_1
         2 -> R.drawable.ic_sleep_2
@@ -20,6 +20,21 @@ fun ImageView.setSleepImage(item: SleepNight) {
         else -> R.drawable.ic_sleep_active
     })
 }
+
+//@BindingAdapter("sleepImage")
+//fun ImageView.setSleepImage(item: SleepNight?) {
+//    setImageResource(
+//        when (item?.sleepQuality) {
+//            0 -> R.drawable.ic_sleep_0
+//            1 -> R.drawable.ic_sleep_1
+//            2 -> R.drawable.ic_sleep_2
+//            3 -> R.drawable.ic_sleep_3
+//            4 -> R.drawable.ic_sleep_4
+//            5 -> R.drawable.ic_sleep_5
+//            else -> R.drawable.ic_sleep_active
+//        }
+//    )
+//}
 
 @BindingAdapter("sleepDurationFormatted")
 fun TextView.setSleepDurationFormatted(item: SleepNight?) {
